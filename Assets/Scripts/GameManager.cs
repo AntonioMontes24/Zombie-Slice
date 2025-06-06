9122818d0ea77c772620e2212bfaab7b82935735
Ceaser Hernandez <ceaser.r.hernandez@gmail.com>
2025-06-06 15:00:49 -0700

    added gamemanager script, game timer, game pause/unpause code, game menu code

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+    [SerializeField] TMP_Text gameTimerText;
+    [SerializeField] TMP_Text gameScoreText;
+
+
+    public GameObject playerDamageScreen;
+    public Image playerHealthBar;
+    public GameObject player;
+    public PlayerController playerScript;
+
+    public bool isPaused;
+
+    float timeScaleOrig;
+
+    public float gameTimer = 300f;
+    int gameScore;
+    private TimeSpan timeRemaining;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        instance = this;
+        player = GameObject.FindWithTag("Player");
+        playerScript = player.GetComponent<PlayerController>();
+        timeScaleOrig = Time.timeScale;
+
+        timeRemaining = TimeSpan.FromSeconds(gameTimer);
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if(menuActive = null)
+            {
+                statePause();
+                menuActive = menuPause;
+                menuActive.SetActive(isPaused);
+            } else if (menuActive == menuPause)
+            {
+                stateUnpause();
+            }
+        }
+
+        if(timeRemaining.TotalSeconds > 0)
+        {
+            timeRemaining -= TimeSpan.FromSeconds(Time.deltaTime);
+
+            gameTimerText.text = string.Format("{0:D2}:{1:D2}", timeRemaining.Minutes, timeRemaining.Seconds);
+        } else
+        {
+            gameTimerText.text = "00:00";
+        }
+        
+    }
+
+    public void statePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void stateUnpause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = timeScaleOrig;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(false);
+        menuActive = null;
+    }
+
+    public void youLose()
+    {
+        statePause();
+        menuActive = menuLose;
+        menuActive.SetActive(true);
+    }
+
+    public void updateGameScore(int amount)
+    {
+        gameScore += amount;
+        gameScoreText.text = gameScore.ToString("F0");
+    }
+
+
+}
