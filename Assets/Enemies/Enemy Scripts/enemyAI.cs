371b9b54ed3d0858a5951c810fdae6dc923ac146
William Brevard <william@nolimitgamez.com>
2025-06-09 00:38:13 -0700

    Updated variant 1 zombie
    Zombie enemy has idle animation, will face player when in range and move towards player while playing walking animation. Still need to figure out how to trigger attack animation when close enough to player. Also, needs to be checked for take damage (should play gethit animation) and dieing (should play death animation, wait 3 seconds, then destroy gameobject)

--- Assets/Enemies/Enemy Scripts/enemyAI.cs
+++ Assets/Enemies/Enemy Scripts/enemyAI.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,40 +11,44 @@ public class enemyAI : MonoBehaviour
     [SerializeField] NavMeshAgent agent;
     [SerializeField] int HP; //Enemy HP variable
     [SerializeField] int facePlayerSpeed;
-   
-   // public Transform target;
-    //public Rigidbody rb;
+    int zHP;
+    public Animator anim; //Animator controller
     Color colorOrig; //Original color for hit feedback
 
     bool playerInRange; //Will check to see if player in range for attack
     Vector3 playerDir; //Will be used to direct towards player
+    public GameObject playerObject;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         colorOrig = model.material.color;
-       
+        anim = GetComponent<Animator>();
         //How will the enemy affect the gamemanger to keep track of the number of enemies left for the goal of the game?
     }
 
     // Update is called once per frame
     void Update()
     {
-       playerDir = zGameManager.instance.player.transform.position - transform.position; //Track player - calling zGameManager - created to test movement
+        playerDir = zGameManager.instance.player.transform.position - transform.position; //Track player - calling zGameManager - created to test movement
 
         if (playerInRange)
         {
+            anim.SetBool("playerInRange", true);
+            //z1Attack();
+
             agent.SetDestination(zGameManager.instance.player.transform.position);
             if (agent.remainingDistance <= agent.stoppingDistance)
             {
                 FacePlayer();
+                // anim.SetBool("playerInRange", true);
             }
         }
-    }
-
-    public void takeDamage(int amount)
-    {
-        HP -= amount; //Reduce enemy HP by amount of damage taken from player/weapon
+        else
+        {
+            anim.SetBool("playerInRange", false);
+            anim.SetInteger("distance", 1);
+        }
     }
 
     /*MOVEMENT*/
@@ -68,16 +74,20 @@ public class enemyAI : MonoBehaviour
         }
     }
 
-  
+
 
     /*DAMAGE*/
     public void takeDamge(int amount)
     {
-        HP -= amount;
+        HP -= amount; //Reduce enemy HP by amount of damage taken from player/weapon
+        zHP = HP;
+        anim.SetTrigger("Hit");
+        anim.SetInteger("HP", -zHP);
 
         if (HP <= 0)
         {
-            Destroy(gameObject);
+            StartCoroutine(deathAnim());
+            //Destroy(gameObject);
         }
         else
         {
@@ -90,6 +100,26 @@ public class enemyAI : MonoBehaviour
         model.material.color = Color.red;
         yield return new WaitForSeconds(0.1f);
         model.material.color = colorOrig;
+    }
+
+    IEnumerator deathAnim()
+    {
+        anim.SetBool("isDead", true);
+        yield return new WaitForSeconds(3.0f);
+        Destroy(gameObject);
+    }
+    private void z1Attack()
+    {
+        //anim.SetInteger("distance", 0);
+        anim.SetTrigger("Attack");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == playerObject)
+        {
+            z1Attack();
+        }
     }
 
     //Future possibility is to slow the enemy down with each hit
