20a3f571c2ee9f3553d8abd2df04ac7c88c38249
William Brevard <william@nolimitgamez.com>
2025-06-10 08:22:01 -0700

    Zombie 1 progress
    Cleaned up script
    Implemented walking/stopping/attacking animation transitions
    Need to test damage dealt and damage received as well as death and death anim

--- Assets/Enemies/Enemy Scripts/enemyAI.cs
+++ Assets/Enemies/Enemy Scripts/enemyAI.cs
@@ -17,6 +17,7 @@ public class enemyAI : MonoBehaviour
 
     bool playerInRange; //Will check to see if player in range for attack
     Vector3 playerDir; //Will be used to direct towards player
+    
     public GameObject playerObject;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -31,23 +32,34 @@ public class enemyAI : MonoBehaviour
     void Update()
     {
         playerDir = zGameManager.instance.player.transform.position - transform.position; //Track player - calling zGameManager - created to test movement
-
+        float dist = Vector3.Distance(transform.position, playerObject.transform.position);
         if (playerInRange)
         {
-            anim.SetBool("playerInRange", true);
+
             //z1Attack();
 
             agent.SetDestination(zGameManager.instance.player.transform.position);
             if (agent.remainingDistance <= agent.stoppingDistance)
             {
                 FacePlayer();
-                // anim.SetBool("playerInRange", true);
+                
             }
+            //Check if enemy is within melee attack range
+            if (dist <= 1.75)
+            {
+                z1Attack();//Trigger attack animation
+            }
+            else if (dist >= 1.76)
+            {
+                anim.SetBool("AttackRange", false);//Trigger animator attack to stop until back in range
+                agent.isStopped = false;
+            }
+          
         }
         else
         {
-            anim.SetBool("playerInRange", false);
-            anim.SetInteger("distance", 1);
+
+            anim.SetBool("AttackRange", false);
         }
     }
 
@@ -63,6 +75,8 @@ public class enemyAI : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             playerInRange = true;
+            agent.isStopped = false;
+            anim.SetBool("playerInRange", true);//Trigger walk animation
         }
     }
 
@@ -71,6 +85,8 @@ public class enemyAI : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             playerInRange = false;
+            agent.isStopped = true;
+            anim.SetBool("playerInRange", false);//Trigger idle animation
         }
     }
 
@@ -82,11 +98,11 @@ public class enemyAI : MonoBehaviour
         HP -= amount; //Reduce enemy HP by amount of damage taken from player/weapon
         zHP = HP;
         anim.SetTrigger("Hit");
-        anim.SetInteger("HP", -zHP);
+        anim.SetInteger("HP", zHP);
 
         if (HP <= 0)
         {
-            StartCoroutine(deathAnim());
+            StartCoroutine(deathAnim());//Trigger death animation
             //Destroy(gameObject);
         }
         else
@@ -98,29 +114,25 @@ public class enemyAI : MonoBehaviour
     IEnumerator flashRed()
     {
         model.material.color = Color.red;
+        anim.SetTrigger("Hit");//Trigger hit animation
         yield return new WaitForSeconds(0.1f);
         model.material.color = colorOrig;
     }
-
+    //Play hit animation when taking damage
     IEnumerator deathAnim()
     {
         anim.SetBool("isDead", true);
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(3.0f);//Play death animation then remove from area (destroy object) after 3 seconds. Could remove this if we want to have bodies remain
         Destroy(gameObject);
     }
+    //Intention is to set the distance parameter in the animator to 0 to trigger attack animation
     private void z1Attack()
     {
-        //anim.SetInteger("distance", 0);
-        anim.SetTrigger("Attack");
+        agent.isStopped = true;
+        anim.SetBool("AttackRange", true);
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject == playerObject)
-        {
-            z1Attack();
-        }
-    }
+   
 
     //Future possibility is to slow the enemy down with each hit
 
---
+++ Assets/Enemies/Enemy Scripts/zDamage.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface zDamage
+{
+    void takeDamage(int amount);
+}
