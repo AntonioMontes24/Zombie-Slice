5e0066291531992c1e633edcedbb540f33f91b06
Antonio Montes <montescases@gmail.com>
2025-06-30 13:08:38 -0700

    Checking PR requiriment

--- Assets/Player/Scripts/PlayerWeaponController.cs
+++ Assets/Player/Scripts/PlayerWeaponController.cs
@@ -104,7 +104,6 @@ public class PlayerWeaponManager : MonoBehaviour
         EquipWeapon(gunList.Count - 1);
     }
 
-
     public void HandleShooting()//Handles Shooting
     {
         if (movement != null && movement.canSprint && Input.GetButton("Sprint"))
