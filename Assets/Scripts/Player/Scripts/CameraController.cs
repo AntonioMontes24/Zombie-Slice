27ee1139011bc448e7012b67df2f92a867afe32f
Ceaser Hernandez <ceaser.r.hernandez@gmail.com>
2025-07-11 16:50:25 -0700

    Added ui for keybindings, set up script, still does not rebind keys its WIP, setup mouse sensitivity slider - working. Input field also works in changing mouse sense - reset button works.

--- Assets/Scripts/Player/Scripts/CameraController.cs
+++ Assets/Scripts/Player/Scripts/CameraController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float sens = 100f;
+    public float mouseSensitivity = 3.5f;
     public float lockVertMin = -90f;
     public float lockVertMax = 90f;
     public bool invertY = false;
@@ -18,14 +18,18 @@ public class CameraController : MonoBehaviour
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        mouseSensitivity = PlayerPrefs.GetFloat("MouseSensitivity", 3.5f);
+        MouseSensController.OnSensChanged += UpdateSensitivity;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         // get input
-        float mouseX = Input.GetAxis("Mouse X") * sens * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * sens * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         // give option to invert mouse look up and down
         if (invertY)
@@ -34,15 +38,24 @@ public class CameraController : MonoBehaviour
             rotX -= mouseY;
 
         // clamp camera on the x-axis 
-        rotX = Mathf.Clamp(rotX, lockVertMin, lockVertMax);
+        //rotX = Mathf.Clamp(rotX, lockVertMin, lockVertMax);
+
+        rotX = Mathf.Clamp(rotX, -90f, 90f);
 
         // rotate camera on x-axis to look up and down
         // we now rotate pitchTarget (CameraHolder) instead of this object directly
         if (pitchTarget != null)
             pitchTarget.localRotation = Quaternion.Euler(rotX, 0, 0);
 
+        transform.localRotation = Quaternion.Euler(rotX, 0f, 0f);
         // rotate player on y-axis to look left and right
         playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    private void UpdateSensitivity(float newSens)
+    {
+        mouseSensitivity = newSens;
+        Debug.Log($"new sens: {mouseSensitivity}");
     }
 
 }
