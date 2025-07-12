a1dc65ecdd8e0e5dad6ad32483383af9c803aa91
Antonio Montes <montescases@gmail.com>
2025-07-12 10:07:40 -0700

    Camera bug fixed moving weirdly
    Camera bug fixed moving weirdly

--- Assets/Scripts/Player/Scripts/CameraController.cs
+++ Assets/Scripts/Player/Scripts/CameraController.cs
@@ -31,24 +31,13 @@ public class CameraController : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        // give option to invert mouse look up and down
-        if (invertY)
-            rotX += mouseY;
-        else
-            rotX -= mouseY;
+        //give option to invert mouse look up and down
+        rotX += invertY ? mouseY : -mouseY;
+        rotX = Mathf.Clamp(rotX, lockVertMin, lockVertMax);// clamp camera on the x axis 
 
-        // clamp camera on the x-axis 
-        //rotX = Mathf.Clamp(rotX, lockVertMin, lockVertMax);
-
-        rotX = Mathf.Clamp(rotX, -90f, 90f);
-
-        // rotate camera on x-axis to look up and down
-        // we now rotate pitchTarget (CameraHolder) instead of this object directly
         if (pitchTarget != null)
-            pitchTarget.localRotation = Quaternion.Euler(rotX, 0, 0);
+            pitchTarget.localRotation = Quaternion.Euler(rotX, 0f, 0f);
 
-        transform.localRotation = Quaternion.Euler(rotX, 0f, 0f);
-        // rotate player on y-axis to look left and right
         playerBody.Rotate(Vector3.up * mouseX);
     }
 
