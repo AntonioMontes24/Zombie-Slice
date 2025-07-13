373f4d89815f7f1f0e39de56ec2133a57423389d
Antonio Montes <montescases@gmail.com>
2025-07-13 00:04:03 -0700

    Revert "William (#74)"
    This reverts commit 39784371bea192f75565a9593d9e314e534611ea, reversing
    changes made to 27ee1139011bc448e7012b67df2f92a867afe32f.

--- Assets/Scripts/Player/Scripts/PlayerController.cs
+++ Assets/Scripts/Player/Scripts/PlayerController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour, IOpen//Added open interface for crypt door
 {
     [SerializeField] private PlayerMovement playerMovement;
-    //[SerializeField] private PlayerMoveRebind playerMovement;
     [SerializeField] private PlayerWeaponManager weaponManager;
     [SerializeField] private PlayerHealth playerHealth;
     [SerializeField] public GameObject armsModel;
