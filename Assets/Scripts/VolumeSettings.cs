945540659bb8edbe4192f58c761b477f22e96cc4
Ceaser Hernandez <ceaser.r.hernandez@gmail.com>
2025-07-13 11:29:25 -0700

    Commiting so I can pull latest pushes

--- Assets/Scripts/VolumeSettings.cs
+++ Assets/Scripts/VolumeSettings.cs
@@ -14,12 +14,21 @@ public class VolumeSettings : MonoBehaviour
     public const string MIXER_SFX = "SFX";
     public const string MIXER_MENU = "Menu";
 
-    public void Awake()
+    private void OnEnable()
     {
-        musicSlider.value = PlayerPrefs.GetFloat(AudioManager.MUSIC_KEY, 1f);
-        sfxSlider.value = PlayerPrefs.GetFloat(AudioManager.SFX_KEY, 1f);
-        menuSlider.value = PlayerPrefs.GetFloat(AudioManager.MENU_KEY, 1f);
+        if(AudioManager.instance == null)
+        {
+            Debug.LogError("VolumeSettings: Audiomanager instance not found!!");
+            return;
+        }
 
+        if(musicSlider != null) musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        if(sfxSlider != null) sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+        if(menuSlider != null) menuSlider.onValueChanged.AddListener(SetMenuVolume);
+
+        if(musicSlider != null) musicSlider.value = AudioManager.instance.GetMusicVolume();
+        if(sfxSlider != null) sfxSlider.value = AudioManager.instance.GetSFXVolume();
+        if(menuSlider != null) menuSlider.value = AudioManager.instance.GetMenuVolume();
 
 
     }
@@ -30,13 +39,13 @@ public class VolumeSettings : MonoBehaviour
 
     }
 
-    private void OnEnable()
-    {
-        musicSlider.onValueChanged.AddListener(SetMusicVolume);
-        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
-        menuSlider.onValueChanged.AddListener(SetMenuVolume);
+    //private void OnEnable()
+    //{
+    //    musicSlider.onValueChanged.AddListener(SetMusicVolume);
+    //    sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+    //    menuSlider.onValueChanged.AddListener(SetMenuVolume);
 
-    }
+    //}
 
     private void OnDisable()
     {
@@ -44,27 +53,55 @@ public class VolumeSettings : MonoBehaviour
         //PlayerPrefs.SetFloat(AudioManager.SFX_KEY, sfxSlider.value);
         //PlayerPrefs.SetFloat(AudioManager.MENU_KEY, menuSlider.value);
 
-        musicSlider.onValueChanged.RemoveListener(SetMusicVolume);
-        sfxSlider.onValueChanged.RemoveListener(SetSFXVolume);
-        menuSlider.onValueChanged.RemoveListener(SetMenuVolume);
+        if(musicSlider != null) musicSlider.onValueChanged.RemoveListener(SetMusicVolume);
+        if (sfxSlider != null) sfxSlider.onValueChanged.RemoveListener(SetSFXVolume);
+        if (menuSlider != null) menuSlider.onValueChanged.RemoveListener(SetMenuVolume);
+
     }
 
     void SetMusicVolume(float volume)
     {
-        mixer.SetFloat(MIXER_MUSIC, Mathf.Log10(volume) * 20);
+
+        float targetVolume = (volume > 0) ? Mathf.Log10(volume) * 20 : -80f;
+        mixer.SetFloat(MIXER_MUSIC, targetVolume);
         PlayerPrefs.SetFloat(AudioManager.MUSIC_KEY, volume);
+        PlayerPrefs.Save();
     }
 
     void SetSFXVolume(float volume)
     {
-        mixer.SetFloat(MIXER_SFX, Mathf.Log10(volume) * 20);
+        float targetVolume = (volume > 0) ? Mathf.Log10(volume) * 20 : -80f;
+        mixer.SetFloat(MIXER_SFX, targetVolume);
         PlayerPrefs.SetFloat(AudioManager.SFX_KEY, volume);
+        PlayerPrefs.Save();
     }
 
     void SetMenuVolume(float volume)
     {
-        mixer.SetFloat(MIXER_MENU, Mathf.Log10(volume) * 20);
+        float targetVolume = (volume > 0) ? Mathf.Log10(volume) * 20 : -80f;
+        mixer.SetFloat(MIXER_MENU, targetVolume);
         PlayerPrefs.SetFloat(AudioManager.MENU_KEY, volume);
+        PlayerPrefs.Save();
+    }
+
+    public void ResetVolumesToDefault()
+    {
+        PlayerPrefs.DeleteKey(AudioManager.MENU_KEY);
+        PlayerPrefs.DeleteKey(AudioManager.MUSIC_KEY);
+        PlayerPrefs.DeleteKey(AudioManager.SFX_KEY);
+        PlayerPrefs.Save();
+
+        musicSlider.value = 1f;
+        sfxSlider.value = 1f;
+        menuSlider.value = 1f;
+
+        if(AudioManager.instance != null)
+        {
+            AudioManager.instance.ApplySavedVolumeToMixer();
+        }
+
+
+
     }
 
 }
