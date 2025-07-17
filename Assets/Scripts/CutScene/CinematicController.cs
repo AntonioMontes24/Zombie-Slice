66cc6b35176620adf8d38c7ae083d5cdc46fce54
William Brevard <william@nolimitgamez.com>
2025-07-16 18:39:36 -0700

    Cutscene Commit #4
    Added Cutscene audio track
    Fade out at end of track added

--- Assets/Scripts/CutScene/CinematicController.cs
+++ Assets/Scripts/CutScene/CinematicController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
@@ -7,8 +8,15 @@ public class CinematicController : MonoBehaviour
     public PlayableDirector director;
     public string nextSceneName;
     public bool useTimelineSignal = false;
-
+    private bool isFadingOut;
+    private float fadeTimer;
+    private float initialVolume = 1f;
     private bool hasPlayed = false;
+    private float fadeDuration = 2f;
+
+    [Header("CutScene Music")]
+    public AudioSource musicSource;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,6 +24,22 @@ public class CinematicController : MonoBehaviour
         {
             director.Play();
             director.stopped += OnTimeLineFinished;
+        }
+
+        if (musicSource != null && musicSource.clip != null)
+        {
+            initialVolume = musicSource.volume;
+            musicSource.Play();
+        }
+    }
+
+    public void TriggerMusicFade()
+    {
+        if (!isFadingOut && musicSource != null)
+        {
+            isFadingOut = true;
+            fadeTimer = 1f;
+            initialVolume = musicSource.volume;
         }
     }
 
@@ -35,6 +59,11 @@ public class CinematicController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isFadingOut && musicSource != null)
+        {
+            fadeTimer += Time.deltaTime;
+            float progress = Mathf.Clamp01(fadeTimer / fadeDuration);
+            musicSource.volume = Mathf.Lerp(initialVolume, 0f, progress);
+        }
     }
 }
